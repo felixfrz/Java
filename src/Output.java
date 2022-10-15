d0ac72cabcab085a104872fcb59fcc59603cf551
Felix <felix@felixs-mbp.home>
2022-10-15 15:02:47 +0100

    Output

---
+++ src/Output.java
@@ -0,0 +1,32 @@
+public class Output {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+
+
+        /*
+         
+* Print Text
+? You learned from the previous chapter that you can use the println() method to output values or print text in Java:
+! System.out.println("Hello World!");
+? You can add as many println() methods as you want. Note that it will aa new line for each method:
+! System.out.println("Hello World!");
+! System.out.println("I am learning Java.");
+! System.out.println("It is awesome!");
+
+*Double Quotes
+? When you are working with text, it must be wrapped inside double quotations marks "".If you forget the double quotes, an error occurs:
+! System.out.println("This sentence will work!");
+! System.out.println(This sentence will produce an error);
+
+
+*The Print() Method
+? There is also a print() method, which is similar to println().
+? The only difference is that it does not insert a new line at the end of the output:
+! System.out.print("Hello World! ");
+! System.out.print("I will print on the same line.");
+
+         */
+    }
+}
