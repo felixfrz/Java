20cd0d18d728212015440b17610e4b7a62452ac0
Felix <felix@felixs-mbp.home>
2022-10-15 14:36:16 +0100

    Syntax

--- src/Syntax.java
+++ src/Syntax.java
@@ -1,5 +1,23 @@
 public class Syntax {
+   /* 
+   ! Java Syntax
+? In the previous chapter, we created a Java file called Syntax.java, and we used the following code to print "Hello World" to the screen:
+*/
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello World");
     }
+
+/* 
+!     Every line of code that runs in Java must be inside a class. In our example, we named the class Syntax. A class should always start with an uppercase first letter.
+
+* Note: Java is case-sensitive: "MyClass" and "myclass" has different meaning.
+
+*The main Method
+! The main() method is required and you will see it in every Java program:
+!Any code inside the main() method will be executed. Don't worry about the keywords before and after main. You will get to know them bit by bit while reading this tutorial.
+
+! For now, just remember that every Java program has a class name which must match the filename, and that every program must contain the main() method.
+
+*/ 
+
 }
