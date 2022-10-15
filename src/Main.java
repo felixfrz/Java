777173f082166278d477a88c476a4c6ba25c2696
Felix <felix@felixs-mbp.home>
2022-10-15 14:14:18 +0100

    Rename class name to Main

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        String name = "Felix";
+        System.out.println("Hello, World! " + name);
+        System.out.println(2+3);
+        for(int i = 0; i <= 10; i++){
+            System.out.println(i);
+        }
+    }
+}
